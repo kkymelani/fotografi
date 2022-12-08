360714c011b789690a9d88d3850659d3aa62b910
kkymelani <kikymelani30@gmail.com>
2022-12-08 14:50:34 +0700

    delete contactus

--- contact-us.php
+++ contact-us.php
@@ -2,20 +2,6 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(isset($_POST['send']))
-{
-$name=$_POST['fullname'];
-$email=$_POST['email'];
-$contactno=$_POST['contactno'];
-$message=$_POST['message'];
-$sql1="INSERT INTO contactus (nama_visit,email_visit,telp_visit,pesan) VALUES('$name','$email','$contactno','$message')";
-$lastInsertId = mysqli_query($koneksidb, $sql1);
-if($lastInsertId){
-	$msg="Pesan Terkirim. Kami akan menghubungi anda secepatnya.";
-}else{
-	$error="Terjadi Kesalahan! Silahkan coba lagi.";
-}
-}
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
